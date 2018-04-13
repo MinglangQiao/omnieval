778cd7c503d561ebb85b3f1c5954a5ead76cdfdf
minglangqiao <minglangqiao@buaa.edu.cn>
2018-04-13 16:51:01 +0800

    add yuhang remap

---
+++ remap/build/CMakeFiles/remap.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/remap.dir/remap.cpp.o"
+  "CMakeFiles/remap.dir/home/ml/omnieval/yuv/yuv_helper.cpp.o"
+  "CMakeFiles/remap.dir/home/ml/omnieval/map_utils/map_utils.cpp.o"
+  "CMakeFiles/remap.dir/home/ml/omnieval/panomapper/panomapper.cpp.o"
+  "../remap.pdb"
+  "../remap"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/remap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
